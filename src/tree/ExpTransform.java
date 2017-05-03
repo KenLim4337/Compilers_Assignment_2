56223f36902669e03b2c7505d1422453a381ebf8
KenLim4337 <lkyoong428@gmail.com>
2017-05-03 21:49:23 +1000

    Parsing and Static checking complete

---
+++ src/tree/ExpTransform.java
@@ -0,0 +1,23 @@
+package tree;
+
+/**
+ * interface ExpTransform - Handles visitor pattern for transforming expressions.
+ * @version $Revision: 22 $  $Date: 2014-05-20 15:14:36 +1000 (Tue, 20 May 2014) $
+ */
+public interface ExpTransform<ResultType> {
+    ResultType visitErrorExpNode(ExpNode.ErrorNode node);
+    ResultType visitConstNode(ExpNode.ConstNode node);
+    ResultType visitIdentifierNode(ExpNode.IdentifierNode node);
+    ResultType visitVariableNode(ExpNode.VariableNode node);
+    ResultType visitReadNode(ExpNode.ReadNode node);
+    ResultType visitOperatorNode(ExpNode.OperatorNode node);
+    ResultType visitArgumentsNode(ExpNode.ArgumentsNode node);
+    ResultType visitDereferenceNode(ExpNode.DereferenceNode node);
+    ResultType visitNarrowSubrangeNode(ExpNode.NarrowSubrangeNode node);
+    ResultType visitWidenSubrangeNode(ExpNode.WidenSubrangeNode node);
+    //Additional
+    ResultType visitRecordNode(ExpNode.RecordNode node);
+    ResultType visitFieldNode(ExpNode.FieldNode node);
+    ResultType visitNewPointerNode(ExpNode.NewPointerNode node);
+    ResultType visitPointerDerefNode(ExpNode.PointerDerefNode node);
+}
